db6576fa7cba197ac390df6062fbf1fc5e7718f8
cheryalabalram <cheryalabalram@yahoo.com>
2023-08-20 18:52:33 +0530

    Spell Updated

--- src/main/java/com/balram/development/PractiseApplication.java
+++ src/main/java/com/balram/development/PractiseApplication.java
@@ -1,19 +1,17 @@
 package com.balram.development;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.stream.IntStream;
-
+import com.balram.development.entity.Employee;
+import com.balram.development.entity.SupplierItemIdentifier;
+import com.balram.development.repo.EmployeeRepository;
+import com.balram.development.repo.PerformanceRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.balram.development.entity.Employee;
-import com.balram.development.entity.SupplierItemIdentifier;
-import com.balram.development.repo.EmployeeRepository;
-import com.balram.development.repo.PerformanceRepo;
+import java.time.Duration;
+import java.time.Instant;
 
 @SpringBootApplication
 public class PractiseApplication {
@@ -36,7 +34,7 @@ public class PractiseApplication {
 	private void loop(){
 		for(int i=0; i < 200000; i++) {
 			System.out.println("Preloading " + repository.save(new Employee("Examine", "Developer" + i)));
-			System.out.println(" Persormance : "+ performanceRepo.save(new SupplierItemIdentifier("buType"+i,"b"+i,"c"+i,"d"+i,"e"+i)));
+			System.out.println(" Performance : "+ performanceRepo.save(new SupplierItemIdentifier("buType"+i,"b"+i,"c"+i,"d"+i,"e"+i)));
 		}
 	}
 
