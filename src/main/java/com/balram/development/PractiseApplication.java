1f1136608dd1e2c19524acddb5f4e3c580c0e602
cheryalabalram <cheryalabalram@yahoo.com>
2023-08-20 18:52:33 +0530

    Spell Updated

--- src/main/java/com/balram/development/PractiseApplication.java
+++ src/main/java/com/balram/development/PractiseApplication.java
@@ -36,7 +36,7 @@ public class PractiseApplication {
 	private void loop(){
 		for(int i=0; i < 200000; i++) {
 			System.out.println("Preloading " + repository.save(new Employee("Examine", "Developer" + i)));
-			System.out.println(" Persormance : "+ performanceRepo.save(new SupplierItemIdentifier("buType"+i,"b"+i,"c"+i,"d"+i,"e"+i)));
+			System.out.println(" Performance : "+ performanceRepo.save(new SupplierItemIdentifier("buType"+i,"b"+i,"c"+i,"d"+i,"e"+i)));
 		}
 	}
 
